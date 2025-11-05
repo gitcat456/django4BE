c8486b9ac0b77cde83de99f672ac68264f4934e8
gitcat456 <okothdenzel65@gmail.com>
2025-11-06 01:17:52 +0300

    this is like the controller file in express where you define the functions to be perfomed on a certain endpoint

--- posts/views.py
+++ posts/views.py
@@ -9,3 +9,5 @@ def post_list(request):
 def create_post(request):
     return JsonResponse({'message': 'Post Created'})
 
+def delete_post(request):
+    return JsonResponse({'success': 'false', 'message': 'Unauthorised to perform this action'})
