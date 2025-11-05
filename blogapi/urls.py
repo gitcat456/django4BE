7fa61bcec158beea68e14a9a74af99eb6a14d0af
gitcat456 <okothdenzel65@gmail.com>
2025-11-06 02:00:41 +0300

    adds an inline comment

--- blogapi/urls.py
+++ blogapi/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/posts/', include('posts.urls')),
+    path('api/posts/', include('posts.urls')), # This prefixes all posts URLs with /api/posts/
+
 ]
