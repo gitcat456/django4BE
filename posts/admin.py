da71ca36a58bda09df61a5eb30295dcb0609c978
gitcat456 <okothdenzel65@gmail.com>
2025-11-06 02:51:11 +0300

    customizes the layout of the admin field

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# Customize how Post appears in admin
+class PostAdmin(admin.ModelAdmin):
+    # Display these fields in the list view
+    list_display = ['id', 'title', 'is_published', 'created_at']
+    
+    # Add search functionality
+    search_fields = ['title', 'content']
+    
+    # Add filters in the sidebar
+    list_filter = ['is_published', 'created_at']
+    
+    # Make fields editable directly from list view
+    list_editable = ['is_published']
+    
+    # Pagination - show 20 items per page
+    list_per_page = 20
+
+# Register with custom options
+admin.site.register(Post, PostAdmin)
