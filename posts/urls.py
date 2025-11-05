a5846642f5fa63107d649004a230b3ee9f2657fc
gitcat456 <okothdenzel65@gmail.com>
2025-11-06 02:07:36 +0300

    minor refactors to the urls

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list),
-    path('create/', views.create_post),
+    path('', views.post_list, name='post-list'),
+    path('create/', views.create_post, name='create-post'),
     path('delete/', views.delete_post),
 ]
