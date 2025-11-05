5759d9da59ab670c5842027cbf6b926a421a7ea4
gitcat456 <okothdenzel65@gmail.com>
2025-11-06 02:08:22 +0300

    use sqlite to setup a simple demo for databse interactions

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import Post
 
+@csrf_exempt
 def post_list(request):
-    
-    posts = [{'id': 1, 'title': 'First Post'}]
-    return JsonResponse(posts, safe=False)
+    if request.method == 'GET':
+        posts = list(Post.objects.values())
+        return JsonResponse(posts, safe=False)
 
-def create_post(request):
-    return JsonResponse({'message': 'Post Created'})
+@csrf_exempt
+def create_post(request): 
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        post = Post.objects.create(
+            title=data['title'],
+            content=data['content'],
+            is_published=data.get('is_published', False)
+        )
+        return JsonResponse({
+            'id': post.id,
+            'title': post.title,
+            'content': post.content,
+            'created_at': post.created_at.isoformat()
+        })
+
 
 def delete_post(request):
     return JsonResponse({'success': 'false', 'message': 'Unauthorised to perform this action'})
