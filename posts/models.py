29f4d9b2f360ab297ce3a472fd4c44743ee20f41
gitcat456 <okothdenzel65@gmail.com>
2026-02-01 23:05:55 +0300

    Creates author model

--- posts/models.py
+++ posts/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
-class Post(models.Model):
-    
+class Author(models.Model):
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.author
+
+class Post(models.Model): 
     title = models.CharField(max_length=200)
     content = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=False)
     
