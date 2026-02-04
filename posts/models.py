cb940ae6863d8676a28ad953bb2b72bbb808511e
gitcat456 <okothdenzel65@gmail.com>
2026-02-04 09:54:48 +0300

    refactors to prevent migration issues

--- posts/models.py
+++ posts/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 class Author(models.Model):
-    author = models.CharField(max_length=100)
+    author_name = models.CharField(max_length=100)
+    bio = models.TextField()
 
     def __str__(self):
-        return self.author
+        return self.author_name
 
 class Post(models.Model): 
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author_name = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts")
     created_at = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=False)
     
