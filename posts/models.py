57908d2855a5929ec1cef9ac574880ce1912ccbb
gitcat456 <okothdenzel65@gmail.com>
2026-04-10 08:45:49 +0300

    creates custom user model

--- posts/models.py
+++ posts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     phone = models.CharField(max_length=15, blank=True)
-    age = models.IntegerField(max_length=2, blank=True)
+    age = models.IntegerField( blank=True)
     
     def __str__(self):
         return self.username
